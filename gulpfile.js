72cb89f6157351ad16894822a26197f766cf46ec
grundel <daniel@grundel.me>
2020-06-04 16:58:25 -0700

    gulp, typescript

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require("gulp");
+var ts = require("gulp-typescript");
+var tsProject = ts.createProject("tsconfig.json");
+
+gulp.task('ts', () => tsProject
+    .src()
+    .pipe(tsProject())
+    .js.pipe(gulp.dest('dist')));
+
+gulp.task('move', () => gulp
+  .src([
+    './src/**/*.html',
+  ], { 
+    // the base option sets the relative root for the set of files,
+    // preserving the folder structure
+    base: './src/' 
+  })
+  .pipe(gulp.dest('dist')));
+
+gulp.task('default', gulp.series('ts', 'move'));
