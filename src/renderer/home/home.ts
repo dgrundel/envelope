dcfc3c742c22828f2b8ce4f78063a95b812a288d
grundel <daniel@grundel.me>
2020-06-05 13:43:10 -0700

    fresh start based on https://github.com/Devtography/electron-react-typescript-webpack-boilerplate

--- src/renderer/home/home.ts
+++
@@ -1,8 +0,0 @@
-const electron = require('electron');
-const ipcRenderer = electron.ipcRenderer;
-
-const statusElem = document.getElementById('status');
-ipcRenderer.on('initial-data', function (event,store) {
-    console.log(event, store);
-    statusElem.textContent = store.ok ? 'ok' : 'uh oh';
-});
