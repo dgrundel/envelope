c8b788a45ccb0040b08bc96852db8874f43e91f2
grundel <daniel@grundel.me>
2020-06-15 17:48:41 -0700

    Field validation that makes sense (?)

--- src/renderer/components/AccountCreate.tsx
+++ src/renderer/components/AccountCreate.tsx
@@ -4,6 +4,7 @@ import { RadioSelectField } from "./forms/RadioSelectField";
 import { getUserAccountTypes, getAccountTypeLabel, AccountType, Account, AccountDataStoreClient } from "@/dataStore/impl/AccountDataStore";
 import { Log } from "@/util/Logger";
 import { Currency } from "@/util/Currency";
+import { FormValidator, CommonValidators, FieldValue } from './forms/FormValidator';
 
 export interface AccountCreateProps {
 
@@ -19,11 +20,24 @@ export interface AccountCreateState {
     balanceError?: any;
 }
 
+const fieldValidators = [{
+    name: 'name',
+    validator: CommonValidators.required('Name')
+}, {
+    name: 'type',
+    validator: CommonValidators.accountType()
+}, {
+    name: 'balance',
+    validator: CommonValidators.currency()
+}];
+
 export class AccountCreate extends React.Component<AccountCreateProps, AccountCreateState> {
+    private readonly validator: FormValidator;
 
     constructor(props: AccountCreateProps) {
         super(props);
 
+        this.validator = new FormValidator(fieldValidators, this.onFieldChange.bind(this));
         this.state = {
             valid: false
         };
@@ -36,7 +50,7 @@ export class AccountCreate extends React.Component<AccountCreateProps, AccountCr
                 label="Account Name"
                 value={this.state.name || ''}
                 error={this.state.nameError}
-                onChange={(e) => this.onNameChange(e)}
+                onChange={(e) => this.validator.setValue('name', e.target.value.trim())}
             />
             <RadioSelectField
                 name="type"
@@ -47,14 +61,14 @@ export class AccountCreate extends React.Component<AccountCreateProps, AccountCr
                     value,
                     label: getAccountTypeLabel(value)
                 }))}
-                onChange={(e) => this.onAccountTypeChange(e)}
+                onChange={(e) => this.validator.setValue('type', e.target.value)}
             />
             <TextField
                 name="balance"
                 label="Current Balance"
                 value={this.state.balance || ''}
                 error={this.state.balanceError}
-                onChange={(e) => this.onBalanceChange(e)}
+                onChange={(e) => this.validator.setValue('balance', e.target.value.trim())}
             />
             <div>
                 <button className="btn" type="submit">
@@ -64,10 +78,21 @@ export class AccountCreate extends React.Component<AccountCreateProps, AccountCr
         </form>;
     }
 
+    onFieldChange(fieldName: string, fieldValue: FieldValue) {
+        const errors = this.validator.errors();
+        const newState = {
+            nameError: errors.name,
+            typeError: errors.type,
+            balanceError: errors.balance
+        };
+        (newState as any)[fieldName] = fieldValue;
+        this.setState(newState);
+    }
+
     onSubmit(e: React.FormEvent<HTMLFormElement>): void {
         e.preventDefault();
         
-        if (this.state.valid) {
+        if (this.validator.allValid()) {
             const balance = Currency.parse(this.state.balance || '');
             const account: Account = {
                 name: this.state.name as string,
@@ -81,40 +106,12 @@ export class AccountCreate extends React.Component<AccountCreateProps, AccountCr
                     Log.debug(created);
                 });
         } else {
-            this.setState(prev => this.validate(prev, {}));
+            const errors = this.validator.errors();
+            this.setState({
+                nameError: errors.name,
+                typeError: errors.type,
+                balanceError: errors.balance
+            });
         }
-    }
-
-    onNameChange(e: React.ChangeEvent<HTMLInputElement>): void {
-        const name = e.target.value.trim();
-        this.setState(prev => this.validate(prev, { name }));
-    }
-
-    onAccountTypeChange(e: React.ChangeEvent<HTMLInputElement>): void {
-        const type = (e.target.value as AccountType);
-        this.setState(prev => this.validate(prev, { type }));
-    }
-    
-    onBalanceChange(e: React.ChangeEvent<HTMLInputElement>): void {
-        const balance = e.target.value;
-        this.setState(prev => this.validate(prev, { balance }));
-    }
-
-    validate(prev: AccountCreateState, updates: any): AccountCreateState {
-        const name = updates.hasOwnProperty('name') ? updates.name : prev.name;
-        const nameValid = name && name.length > 0;
-        updates.nameError = nameValid ? undefined : 'Please enter a name.';
-        
-        const type = updates.hasOwnProperty('type') ? updates.type : prev.type;
-        const typeValid = !!type;
-        updates.typeError = typeValid ? undefined : 'Please select an account type.';
-        
-        const balance = updates.hasOwnProperty('balance') ? updates.balance : prev.balance;
-        const balanceValid = (balance && balance.length) ? Currency.parse(balance).isValid() : true;
-        updates.balanceError = balanceValid ? undefined : 'Hmm, this doesn\'t look like a number.';
-        
-        updates.valid = !!(nameValid && typeValid && balanceValid);
-
-        return updates;
     }
 }
---
+++ src/renderer/components/forms/FormValidator.ts
@@ -0,0 +1,133 @@
+import { getUserAccountTypes } from '@/dataStore/impl/AccountDataStore';
+import { Currency } from '@/util/Currency';
+
+export type FieldValue = string | string[] | undefined;
+export type ValidationResult = string | boolean;
+export type FieldValidator = (value?: FieldValue) => ValidationResult;
+export type FieldValueMap = Record<string, FieldValue>;
+export type FieldErrorMap = Record<string, string>;
+export type ChangeHandler = (fieldName: string, fieldValue: FieldValue) => void;
+
+export interface ValidatedField {
+    name: string;
+    value?: FieldValue;
+    validator: FieldValidator;
+}
+
+interface FieldRecord extends ValidatedField {
+    value?: FieldValue;
+    valid: boolean;
+    error?: string;
+}
+
+type FieldMap = Record<string, FieldRecord>;
+
+export class CommonValidators {
+    static chain(...validators: FieldValidator[]): FieldValidator {
+        return (value?: FieldValue) => {
+            // run each validator until one returns an invalid result
+            for (let i = 0; i < validators.length; i++) {
+                const result = validators[i](value);
+                if (result !== true) {
+                    return result;
+                }
+            }
+            // all passed, return true
+            return true;
+        };
+    }
+
+    static required(fieldLabel: string): FieldValidator {
+        return (value?: FieldValue) => typeof value === 'string' && value.trim().length > 0 ? true : `Please enter a ${fieldLabel}`;
+    }
+
+    static accountType(): FieldValidator {
+        return (value?: FieldValue) => typeof value === 'string' && getUserAccountTypes().findIndex(t => t === value) !== -1 ? true : `Please select an account type.`;
+    }
+    
+    static currency(): FieldValidator {
+        return (value?: FieldValue) => {
+            if (typeof value === 'string') {
+                if (value.trim().length > 0 && !Currency.parse(value).isValid()) {
+                    return `Hmm, this doesn't look like a number.`
+                }
+            }
+            return true;
+        };
+    }
+    
+};
+
+export class FormValidator {
+    private readonly touched: Record<string, boolean>;
+    private readonly fields: FieldMap;
+    private readonly changeHandler?: ChangeHandler;
+
+    constructor(fields: ValidatedField[], changeHandler?: ChangeHandler) {
+        this.touched = {};
+        this.fields = fields.reduce((fields: FieldMap, field: ValidatedField) => {
+            fields[field.name] = this.validateField({
+                ...field,
+                valid: false
+            });
+            return fields;
+        }, {});
+        this.changeHandler = changeHandler;
+    }
+
+    setValue(fieldName: string, fieldValue: FieldValue) {
+        const field = this.fields[fieldName];
+        if (field) {
+            field.value = fieldValue;
+            this.validateField(field, true);
+
+            if (this.changeHandler) {
+                this.changeHandler(fieldName, fieldValue);
+            }
+        }
+    }
+
+    allValid(touch: boolean = true): boolean {
+        return Object.keys(this.fields)
+            .reduce((allValid: boolean, fieldName: string) => {
+                const field = this.fields[fieldName];
+                this.validateField(field, touch);
+                return allValid && field.valid;
+            }, true);
+    }
+
+    values(): FieldValueMap {
+        return Object.keys(this.fields)
+            .reduce((map: FieldValueMap, fieldName: string) => {
+                map[fieldName] = this.fields[fieldName].value;
+                return map;
+            }, {});
+    }
+
+    errors(): FieldErrorMap {
+        return Object.keys(this.fields)
+            .reduce((map: FieldErrorMap, fieldName: string) => {
+                const error = this.fields[fieldName].error;
+                if (error) {
+                    map[fieldName] = error;
+                }
+                return map;
+            }, {});
+    }
+
+    private validateField(field: FieldRecord, touch: boolean = false): FieldRecord {
+        if (touch) {
+            this.touched[field.name] = true;
+        }
+        const touched = this.touched[field.name] === true;
+        const result = field.validator(field.value);
+        field.valid = result === true;
+        // only _show_ the errors if the field has been touched
+        if (touched && typeof result === 'string') {
+            field.error = result;
+        } else {
+            field.error = undefined;
+        }
+        return field; // for ease of use
+    }
+}
