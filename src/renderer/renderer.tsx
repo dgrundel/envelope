1ee5da308c50c53026682ffcea32ef2afd549109
grundel <daniel@grundel.me>
2020-06-23 13:21:48 -0700

    add react-redux types and set up provider

--- src/renderer/renderer.tsx
+++ src/renderer/renderer.tsx
@@ -1,9 +1,13 @@
 import '@public/style.scss';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
 import { App } from './components/App';
+import { ReduxStore } from './store/store';
 
 ReactDOM.render(
-    <App/>,
+    <Provider store={ReduxStore}>
+        <App/>
+    </Provider>,
     document.getElementById('root')
 );
