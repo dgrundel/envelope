c2f68c045d18f10930683905370f3a6fbd1f6e5e
grundel <daniel@grundel.me>
2020-06-23 14:00:15 -0700

    install redux-thunk

--- src/renderer/store/store.ts
+++ src/renderer/store/store.ts
@@ -1,8 +1,9 @@
-import { combineReducers, createStore } from "redux";
+import { combineReducers, createStore, applyMiddleware } from "redux";
+import thunk from 'redux-thunk';
 import { accounts } from './reducers/Accounts';
 
 const reducer = combineReducers({
     accounts
 });
 
-export const ReduxStore = createStore(reducer);
+export const ReduxStore = createStore(reducer, applyMiddleware(thunk));
