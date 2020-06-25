53119bdc0588519c3745497a70ad7961dc2b1ef6
grundel <daniel@grundel.me>
2020-06-25 14:50:07 -0700

    remove box style

--- src/renderer/components/Box.tsx
+++ src/renderer/components/Box.tsx
@@ -2,19 +2,14 @@ import * as React from "react";
 
 import '@public/components/Box.scss';
 
-export enum BoxStyle {
-    success = 'box-success',
-}
-
 export interface BoxProps {
-    boxStyle?: BoxStyle;
     heading?: string;
     children: any;
 }
 
 export class Box extends React.Component<BoxProps, {}> {
     render() {
-        return <div className={'box ' + (this.props.boxStyle || '')}>
+        return <div className="box">
             {this.props.heading ? <h2 className="box-heading">{this.props.heading}</h2> : ''}
             {this.props.children}
         </div>;
