c174b9077a95afec619b2ff13e6f19a2279e65ae
grundel <daniel@grundel.me>
2020-07-01 12:51:45 -0700

    somewhat helpful welcome message

--- src/renderer/components/DashboardPage.tsx
+++ src/renderer/components/DashboardPage.tsx
@@ -8,7 +8,17 @@ export interface DashboardPageProps {
 export class DashboardPage extends React.Component<DashboardPageProps, {}> {
     render() {
         return <>
-            <Box>Welcome.</Box>
+            <Box heading="Getting Started">
+                <p>Welcome to Envelope! Here are a few steps to get you going:</p>
+
+                <ol>
+                    <li>Click <strong>Accounts</strong> and add each of your bank and credit card accounts.</li>
+                    <li>Click <strong>Envelopes</strong> and create a few envelopes for your budget.</li>
+                    <li>Move some money from your <strong>Available Funds Envelope</strong> into your budget envelopes.</li>
+                    <li><strong>Drag and Drop</strong> a CSV file exported from your bank's website to import bank and credit card transactions.</li>
+                </ol>
+            </Box>
+            
         </>;
     }
 }
