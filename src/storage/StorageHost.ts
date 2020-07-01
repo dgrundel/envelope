05086e1dbade526d3a0d3e8184ceaf022f33efe7
grundel <daniel@grundel.me>
2020-06-30 18:20:01 -0700

    throttled save mechanism

--- src/storage/StorageHost.ts
+++ src/storage/StorageHost.ts
@@ -5,27 +5,31 @@ import { Log } from '@/util/Logger';
 import { resolve } from 'dns';
 
 const ENCODING = 'utf8';
-const BACKUP_SUFFIX = '.bak';
+const SAVE_THROTTLE_TIMEOUT = 5 * 1000;
 
 export interface StorageApi {
     get: (key: string) => Promise<string>;
     set: (key: string, value: string) => Promise<void>;
     remove: (key: string) => Promise<void>;
+    save(): Promise<void>;
 }
 
 export class StorageHost implements StorageApi {
     private readonly name: string;
     private readonly filePath: string;
     private data: any = {};
-    private _queue: Promise<any>;
+    private queue: Promise<any>;
+    
+    private lastSave: number = 0;
+    private saveTimeout?: NodeJS.Timeout;
 
     constructor(name: string) {
         this.name = name;
         this.filePath = path.join(app.getPath('userData'), `${name}.json`);
-        this._queue = this.load();
+        this.queue = this.load();
     }
 
-    load(): Promise<void> {
+    private load(): Promise<void> {
         return new Promise(resolve => {
             fs.access(this.filePath, fs.constants.F_OK, err => err ? resolve(false) : resolve(true));
         })
@@ -43,29 +47,51 @@ export class StorageHost implements StorageApi {
         });
     }
 
-    save(): Promise<void> {
+    private write(): Promise<void> {
         const json = JSON.stringify(this.data);
         return fs.promises.writeFile(this.filePath, json, { encoding: ENCODING });
     }
 
+    private throttledSave(): Promise<void> {
+        if (this.saveTimeout) {
+            clearTimeout(this.saveTimeout);
+        }
+
+        const elapsedTime = Date.now() - this.lastSave;
+        if (elapsedTime > SAVE_THROTTLE_TIMEOUT) {
+            // actually save the data
+            const json = JSON.stringify(this.data);
+            return this.write().then(() => { this.lastSave = Date.now() });
+                
+        } else {
+            this.saveTimeout = setTimeout(this.write.bind(this), SAVE_THROTTLE_TIMEOUT - elapsedTime);
+            return Promise.resolve();
+        }
+    }
+
+    save(): Promise<void> {
+        return this.queue = this.queue
+            .then(() => this.write());
+    }
+
     get(key: string): Promise<string> {
-        return this._queue = this._queue
+        return this.queue = this.queue
             .then(() => this.data[key]);
     }
 
     set(key: string, value: string): Promise<void> {
-        return this._queue = this._queue
+        return this.queue = this.queue
             .then(() => {
                 this.data[key] = value;
-                return this.save();
+                return this.throttledSave();
             });
     }
 
     remove(key: string): Promise<void> {
-        return this._queue = this._queue
+        return this.queue = this.queue
             .then(() => {
                 delete this.data[key];
-                return this.save();
+                return this.throttledSave();
             });
     }
 }
