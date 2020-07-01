5a6d073f8a7f061d43e870c4d99b6951efeb3bca
Daniel Grundel <daniel@grundel.me>
2020-07-01 14:52:46 -0700

    Revert "[in progress] adding duplicate import detection"
    This reverts commit 7185b1802eb15b98751d37e4b1dfc11965950c39.
    
    Abandoning this feature pending a refactor of the Wizard component.

--- src/renderer/components/import/ImportWizard.tsx
+++ src/renderer/components/import/ImportWizard.tsx
@@ -3,20 +3,18 @@ import { getAppContext } from '@/renderer/AppContext';
 import { insertTransactions } from '@/renderer/store/actions/Transaction';
 import { CombinedState } from '@/renderer/store/store';
 import { Currency } from '@/util/Currency';
-import { filterOnlyBankAccounts, filterOnlyImportedTransactions } from '@/util/Filters';
+import { filterOnlyBankAccounts } from '@/util/Filters';
 import { Log } from '@/util/Logger';
 import { Account, AccountType, getAccountTypeLabel } from '@models/Account';
 import '@public/components/import/ImportWizard.scss';
 import * as moment from "moment";
 import * as React from "react";
 import { connect } from 'react-redux';
-import { AppPage } from '../App';
-import { SpinnerModal } from '../SpinnerModal';
 import { RowSelect } from '../uiElements/RowSelect';
+import { SpinnerModal } from '../SpinnerModal';
 import { Wizard, WizardApi, WizardStep } from '../uiElements/Wizard';
 import { ImportRowSelect } from './ImportRowSelect';
-import { transactions } from '@/renderer/store/reducers/Transactions';
-import { isEqual } from "lodash";
+import { AppPage } from '../App';
 
 
 export interface Row {
@@ -29,7 +27,6 @@ export interface ImportWizardState {
     invertTransactions: boolean;
     accounts: Account[];
     accountMap: Record<string, Account>;
-    importedTransactions: Transaction[];
 
     accountId?: string;
     dateColumn?: string;
@@ -42,13 +39,8 @@ export interface ImportWizardState {
 
 export interface ImportWizardProps {
     rows: Row[];
-
-    // mapped props from store
     accountMap?: Record<string, Account>;
     accounts?: Account[];
-    importedTransactions?: Transaction[];
-
-    // store actions
     insertTransactions?: (transactionData: TransactionData[]) => Promise<void>;
 }
 
@@ -58,7 +50,7 @@ const errorMessage = (s: string) => <p className="import-wizard-error-message">{
 
 const convertToTransactions = (rows: Row[], invert: boolean, dateColumn: string, amountColumn: string, descriptionColumns: string[], accountId: string): TransactionData[] => {
     return rows.map(row => {
-        const date = new Date(row[dateColumn]);
+        const date = moment(row[dateColumn]).toDate();
         const currency = Currency.parse(row[amountColumn]);
         const amount = invert ? currency.getInverse() : currency;
         const description = descriptionColumns
@@ -131,7 +123,7 @@ const dateFieldSelectStep: WizardStep<ImportWizardState> = {
                 type="radio" 
                 rows={state.rows} 
                 onChange={onChange}
-                columnFilter={(key, value) => !isNaN(Date.parse(value))}
+                columnFilter={(key, value) => moment(value).isValid()}
                 value={api.getState().dateColumn}
                 keyHeading="CSV Column"
                 valueHeading="Sample Value" />
@@ -308,69 +300,6 @@ const invertDebitCreditStep: WizardStep<ImportWizardState> = {
     validate: () => ({ valid: true })
 };
 
-const identifyDuplicatesStep: WizardStep<ImportWizardState> = {
-    render: (state: ImportWizardState, api: WizardApi<ImportWizardState>) => {
-        const accountId = state.accountId as string;
-        const dateColumn = state.dateColumn as string;
-        const descriptionColumns = state.descriptionColumns as string[];
-        const amountColumn = state.amountColumn as string;
-        const invert = state.invertTransactions;
-
-        // const minDate = state.rows.reduce((min: number, row: Row) => {
-        //     return Math.min(min, Date.parse(row[dateColumn]));
-        // }, Infinity);
-
-        // const maxDate = state.rows.reduce((max: number, row: Row) => {
-        //     return Math.max(max, Date.parse(row[dateColumn]));
-        // }, -Infinity);
-
-        // filter out transactions from other accounts and outside of this date range
-        // const existing = state.importedTransactions.filter(t => {
-        //     if (t.accountId !== accountId) {
-        //         return false;
-        //     }
-        //     const millis = t.date.getMilliseconds();
-        //     return millis < maxDate && millis > minDate;
-        // });
-
-        const duplicates = state.rows.filter(row => {
-            return state.importedTransactions.some(t => t.originalRecord && isEqual(t.originalRecord, row));
-        });
-
-        console.log(duplicates.length, duplicates);
-
-        // if (duplicates.length === 0) {
-        //     api.nextStep();
-        // }
-
-        const duplicatesAsTransactions: TransactionData[] = convertToTransactions(duplicates, invert, dateColumn, amountColumn, descriptionColumns, accountId);
-
-        console.log(duplicatesAsTransactions);
-
-        return <div className="import-wizard-summary">
-            <h3>Looks like we may have some duplicates.</h3>
-
-            <table style={{minWidth: '60vw'}}>
-                <thead>
-                    <tr>
-                        <th>Date</th>
-                        <th>Description</th>
-                        <th>Amount</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {duplicatesAsTransactions.map((duplicate, i) => <tr key={i}>
-                        <td>{duplicate.date.toLocaleDateString()}</td>
-                        <td>{duplicate.description}</td>
-                        <td>{duplicate.amount.toFormattedString()}</td>
-                    </tr>)}
-                </tbody>
-            </table>
-        </div>;
-    },
-    validate: () => ({ valid: true })
-};
-
 const summaryStep: WizardStep<ImportWizardState> = {
     render: (state: ImportWizardState) => {
         const accountId = state.accountId as string;
@@ -414,10 +343,9 @@ class Component extends React.Component<ImportWizardProps, ImportWizardState> {
             rows: this.props.rows,
             firstRow: this.props.rows[0],
             invertTransactions: false,
-            accounts: props.accounts!,
-            accountMap: props.accountMap!,
-            importedTransactions: props.importedTransactions!,
-            insertTransactions: props.insertTransactions!,
+            accounts: props.accounts || [],
+            accountMap: props.accountMap || {},
+            insertTransactions: props.insertTransactions!
         };
     }
 
@@ -429,7 +357,6 @@ class Component extends React.Component<ImportWizardProps, ImportWizardState> {
                     firstRow: this.props.rows[0],
                     accounts: this.state.accounts,
                     accountMap: this.state.accountMap,
-                    importedTransactions: this.state.importedTransactions,
                     invertTransactions: false,
                     insertTransactions: this.state.insertTransactions
                 },
@@ -439,7 +366,6 @@ class Component extends React.Component<ImportWizardProps, ImportWizardState> {
                     amountFieldSelectStep,
                     descriptionFieldSelectStep,
                     invertDebitCreditStep,
-                    identifyDuplicatesStep,
                     summaryStep
                 ],
                 onComplete: (wizardState: ImportWizardState) => this.onComplete(wizardState)
@@ -480,10 +406,7 @@ class Component extends React.Component<ImportWizardProps, ImportWizardState> {
 const mapStateToProps = (state: CombinedState, ownProps: ImportWizardProps): ImportWizardProps => ({
     ...ownProps,
     accounts: state.accounts.sortedIds.map(id => state.accounts.accounts[id]),
-    accountMap: state.accounts.accounts,
-    importedTransactions: state.transactions.sortedIds
-        .map(id => state.transactions.transactions[id])
-        .filter(filterOnlyImportedTransactions)
+    accountMap: state.accounts.accounts
 });
 
 export const ImportWizard = connect(mapStateToProps, { insertTransactions })(Component);
