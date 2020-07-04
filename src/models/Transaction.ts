5e04cd8f52a8be1cd76cefb1662fef71c3249aa9
grundel <daniel@grundel.me>
2020-07-04 11:13:04 -0700

    adding reconciled flag

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -7,41 +7,45 @@ export enum TransactionFlag {
 
     // A transaction used to adjust an account balance
     // so that it reconciles with the actual account.
-    Adjustment = 1,
+    Adjustment = 1 << 0,
     
     // A transfer of funds from one account to another
-    Transfer = 2,
+    Transfer = 1 << 1,
     
     // Credit applied to a bank/deposit account
     // Increases account balance
     // For a checking account, this might be something like a paycheck deposit
     // For a savings account, this could be accured interest paid by the bank
-    BankCredit = 4,
+    BankCredit = 1 << 2,
     
     // Debit applied to a bank/deposit account
     // Decreases account balance
     // Generally this is from a purchase, or fees incurred from the bank
-    BankDebit = 8,
+    BankDebit = 1 << 3,
     
     // Credit applied to a credit card account
     // Decreases account balance
     // Generally this is from a payment or perhaps a refund issued by a merchant
-    CreditAccountCredit = 16,
+    CreditAccountCredit = 1 << 4,
     
     // Debit applied to a credit card account
     // Increases account balance
     // This could be from a purchase, an interest charge, or other fees incurred
-    CreditAccountDebit = 32,
+    CreditAccountDebit = 1 << 5,
+
+    // Transaction that is fully reconciled
+    Reconciled = 1 << 6,
 };
 
 const transactionFlagDescriptions = {
     [TransactionFlag.None]: '',
     [TransactionFlag.Adjustment]: 'a balance adjustment',
+    [TransactionFlag.Transfer]: 'a transfer of funds between accounts and/or envelopes',
     [TransactionFlag.BankCredit]: 'a deposit, refund, or other credit that increases the account balance',
     [TransactionFlag.BankDebit]: 'a purchase, bill payment, fee, or other type of debit that decreases the account balance',
     [TransactionFlag.CreditAccountCredit]: 'a payment, refund, or other credit that decreases the account balance',
     [TransactionFlag.CreditAccountDebit]: 'a purchase, fee, or other charge that increases the account balance',
-    [TransactionFlag.Transfer]: 'a transfer of funds between accounts and/or envelopes',
+    [TransactionFlag.Reconciled]: 'reconciled',
 };
 
 export const getTransactionFlagDescription = (f: TransactionFlag): string => transactionFlagDescriptions[f];
