ce719d9db6111ed1f79341e14505bf995a1a2e2a
grundel <daniel@grundel.me>
2020-07-05 15:35:58 -0700

    test remaining reducer functions

--- test/renderer/store/reducers/testTransactions.ts
+++ test/renderer/store/reducers/testTransactions.ts
@@ -2,9 +2,10 @@ import { AccountType } from '@/models/Account';
 import { transactions } from '@/renderer/store/reducers/Transactions';
 import { Currency } from '@/util/Currency';
 import { assert } from 'chai';
-import { addTransaction, addManyTransactions } from '@/renderer/store/actions/Transaction';
+import { addTransaction, addManyTransactions, addTransactionFlags, linkExistingTransactions } from '@/renderer/store/actions/Transaction';
 import { Transaction, TransactionFlag } from '@/models/Transaction';
 import { getIdentifier } from '@/util/Identifier';
+import { unionFlags } from '@/util/Flags';
 
 describe('tranasctions reducer', function() {
     it('should provide usable initial state', function() {
@@ -111,5 +112,85 @@ describe('tranasctions reducer', function() {
         const state = transactions(undefined, addManyTransactions(many));
 
         assert.deepEqual(state.sortedIds, ['a', 'b', 'c', 'd']);
+    });
+
+    it('should correctly merge flags', function() {
+        const _id = 'test-id';
+        const transaction: Transaction = {
+            _id,
+            accountId: 'test-account-id',
+            date: new Date(),
+            amount: Currency.ZERO,
+            description: 'test trans desc',
+            flags: TransactionFlag.BankCredit,
+            linkedTransactionIds: [],
+        };
+        
+        const state = transactions(undefined, addTransaction(transaction));
+
+        assert.deepEqual(state.transactions[_id], transaction);
+        assert.deepEqual(state.sortedIds, [_id]);
+
+        const updated = transactions(state, addTransactionFlags(transaction, TransactionFlag.Transfer));
+
+        assert.deepEqual(updated.transactions[_id], {
+            ...transaction,
+            flags: unionFlags(
+                TransactionFlag.BankCredit, 
+                TransactionFlag.Transfer,
+            ),
+        });
+    });
+
+    it('should correctly link existing transactions', function() {
+        const generateTransaction = (id: string) => ({
+            _id: id,
+            accountId: 'test-account-id',
+            date: new Date(),
+            amount: new Currency(12, 340),
+            description: 'test trans desc',
+            flags: TransactionFlag.BankCredit,
+            linkedTransactionIds: [],
+        });
+
+        const many = [
+            generateTransaction('a'),
+            generateTransaction('b'),
+            generateTransaction('c'),
+            generateTransaction('d'),
+        ];
+        const state = transactions(undefined, addManyTransactions(many));
+
+        assert.deepEqual(state.sortedIds, ['a', 'b', 'c', 'd']);
+
+        const updated = transactions(state, linkExistingTransactions(many));
+
+        assert.sameMembers(updated.transactions['a'].linkedTransactionIds, ['b', 'c', 'd']);
+        assert.sameMembers(updated.transactions['b'].linkedTransactionIds, ['a', 'c', 'd']);
+        assert.sameMembers(updated.transactions['c'].linkedTransactionIds, ['a', 'b', 'd']);
+        assert.sameMembers(updated.transactions['d'].linkedTransactionIds, ['a', 'b', 'c']);
+    });
+
+    it('should correctly merge and remove dupes while linking existing transactions', function() {
+        const generateTransaction = (id: string, links: string[]) => ({
+            _id: id,
+            accountId: 'test-account-id',
+            date: new Date(),
+            amount: new Currency(12, 340),
+            description: 'test trans desc',
+            flags: TransactionFlag.BankCredit,
+            linkedTransactionIds: links,
+        });
+        
+        const x = generateTransaction('x', []);
+        const a = generateTransaction('a', ['x', 'x']);
+        const b = generateTransaction('b', []);
+
+        const state = transactions(undefined, addManyTransactions([x, a, b]));
+        const updated = transactions(state, linkExistingTransactions([a, b]));
+
+        assert.sameMembers(updated.transactions['x'].linkedTransactionIds, []);
+        assert.sameMembers(updated.transactions['a'].linkedTransactionIds, ['x', 'b']);
+        assert.sameMembers(updated.transactions['b'].linkedTransactionIds, ['a']);
     });
 });
