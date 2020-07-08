0551f95c4afb76c1c4c3b28e0f6792e6b2ee193d
grundel <daniel@grundel.me>
2020-07-07 19:20:51 -0700

    account action creators tests

---
+++ test/renderer/store/actions/testAccount.ts
@@ -0,0 +1,47 @@
+import { Account, AccountType } from '@/models/Account';
+import { Transaction, TransactionFlag } from '@/models/Transaction';
+import { AccountAction, createEnvelope } from '@/renderer/store/actions/Account';
+import { addTransaction, TransactionAction, transferFunds } from '@/renderer/store/actions/Transaction';
+import { Currency } from '@/util/Currency';
+import { assert } from 'chai';
+import { mockStore } from '../mockStore';
+import { accounts } from '@/renderer/store/reducers/Accounts';
+
+
+describe('Account actions', function () {
+
+    it('should create envelope without linked accounts', () => {
+        const store = mockStore();
+        store.dispatch(createEnvelope('my envelope'));
+
+        const storeActions = store.getActions();
+        assert.equal(storeActions.length, 1);
+        
+        const action = storeActions[0];
+        assert.equal(action.type, AccountAction.Add);
+        const account = action.account;
+        assert.isNotEmpty(account._id);
+        assert.equal(account.name, 'my envelope');
+        assert.equal(account.type, AccountType.UserEnvelope);
+        assert.deepEqual(account.balance, Currency.ZERO);
+    });
+
+    it('should create envelope _with_ linked accounts', () => {
+        const store = mockStore();
+        store.dispatch(createEnvelope('my envelope', ['a', 'b', 'c']));
+
+        const storeActions = store.getActions();
+        assert.equal(storeActions.length, 1);
+        
+        const action = storeActions[0];
+        assert.equal(action.type, AccountAction.Add);
+        
+        const account = action.account;
+        assert.isNotEmpty(account._id);
+        assert.equal(account.name, 'my envelope');
+        assert.equal(account.type, AccountType.UserEnvelope);
+        assert.sameMembers(account.linkedAccountIds, ['a', 'b', 'c']);
+        assert.deepEqual(account.balance, Currency.ZERO);
+    });
+
+});
