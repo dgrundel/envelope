9b4b02e10f85007b8ea74bdb7dd1a59c72da93ea
grundel <daniel@grundel.me>
2020-07-10 19:23:05 -0700

    Fix broken import, cleanup dead code

--- src/renderer/components/AppHeader.tsx
+++ src/renderer/components/AppHeader.tsx
@@ -1,24 +1,14 @@
 import * as React from "react";
 import { connect } from 'react-redux';
 import { Layout } from './uiElements/Layout';
-import { ActionButton } from '@fluentui/react';
-import { Modal } from '../store/reducers/AppState';
-import { setModal } from '../store/actions/AppState';
-import { AppSettingsModal } from './AppSettingsModal';
 
 export interface HeaderProps {
-    // store actions
-    setModal?: (modal: Modal) => void;
 }
 
 const envelopeIcon = require('@public/images/envelope-icon.svg');
 
 class Component extends React.Component<HeaderProps, {}> {
     render() {
-        // const showSettings = () => {
-        //     this.props.setModal!(<AppSettingsModal/>);
-        // };
-
         return <div id="header">
             <Layout split={2} noMargin noGap>
                 <div>
@@ -26,23 +16,13 @@ class Component extends React.Component<HeaderProps, {}> {
                     <h1 className="header-text">Envelope</h1>
                 </div>
                 <div style={{ textAlign: 'right' }}>
-                    {/* <ActionButton iconProps={{ iconName: 'Settings' }} onClick={showSettings}>
-                        Settings
-                    </ActionButton> */}
                 </div>
             </Layout>
         </div>;
     }
 }
 
-// const mapStateToProps = (state: CombinedState, ownProps: HeaderProps): HeaderProps => {
-//     return {
-//         ...ownProps,
-//     };
-// };
-
 const storeActions = {
-    setModal,
 };
 
 export const AppHeader = connect(null, storeActions)(Component);
